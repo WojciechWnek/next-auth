acdc27e8c19bc5704375ac2c1a13a9b8a9f30227
WojciechWnek <wownek@gmail.com>
2024-09-25 13:18:52 +0200

    Fix tailwind

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,16 +1,9 @@
-import { CardWrapper } from "@/components/auth/card-wrapper";
 import { LoginForm } from "@/components/auth/login-form";
 
 const LoginPage = () => {
 	return (
-		<CardWrapper
-			headerLabel="Welcome back"
-			backButtonLabel="Don't have an account?"
-			backButtonHref="/auth/register"
-			showSocial
-		>
+
 			<LoginForm />
-		</CardWrapper>
 	);
 };
 
