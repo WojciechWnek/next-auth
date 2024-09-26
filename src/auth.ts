aa6af8f37c07f42ea5bf1452cea43dc1a55ba520
WojciechWnek <wownek@gmail.com>
2024-09-26 15:04:04 +0200

    Add oAuth and custom error behavior

--- src/auth.ts
+++ src/auth.ts
@@ -5,6 +5,7 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 import { PrismaClient, UserRole } from "@prisma/client";
 import authConfig from "./auth.config";
 import { getUserById } from "./data/user";
+import { db } from "@/lib/db";
 
 declare module "next-auth" {
 	interface Session {
@@ -23,6 +24,20 @@ declare module "next-auth/jwt" {
 const prisma = new PrismaClient();
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
+	pages: {
+		signIn: "/auth/login",
+		error: "/auth/error",
+	},
+	events: {
+		async linkAccount({ user }) {
+			await db.user.update({
+				where: {
+					id: user.id,
+				},
+				data: { emailVerified: new Date() },
+			});
+		},
+	},
 	callbacks: {
 		async session({ token, session }) {
 			if (token.sub && session.user) {
