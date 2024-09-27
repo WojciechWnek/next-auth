e602adf72815794ba180548c250cf174eeb0c11e
WojciechWnek <wownek@gmail.com>
2024-09-27 17:05:15 +0200

    Add user button with logout option

--- src/components/auth/logout-button.tsx
+++ src/components/auth/logout-button.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-// import { logout } from "@/actions/logout";
+import { logout } from "@/actions/logout";
 
 interface LogoutButtonProps {
 	children?: React.ReactNode;
@@ -8,7 +8,7 @@ interface LogoutButtonProps {
 
 export const LogoutButton = ({ children }: LogoutButtonProps) => {
 	const onClick = () => {
-		// logout();
+		logout();
 	};
 
 	return (
--- src/components/auth/user-button.tsx
+++ src/components/auth/user-button.tsx
@@ -10,17 +10,17 @@ import {
 	DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
-// import { useCurrentUser } from "@/hooks/use-current-user";
+import { useCurrentUser } from "@/hooks/use-current-user";
 import { LogoutButton } from "@/components/auth/logout-button";
 
 export const UserButton = () => {
-	// const user = useCurrentUser();
+	const user = useCurrentUser();
 
 	return (
 		<DropdownMenu>
 			<DropdownMenuTrigger>
 				<Avatar>
-					{/* <AvatarImage src={user?.image || ""} /> */}
+					<AvatarImage src={user?.image || ""} />
 					<AvatarFallback className="bg-sky-500">
 						<FaUser className="text-white" />
 					</AvatarFallback>
